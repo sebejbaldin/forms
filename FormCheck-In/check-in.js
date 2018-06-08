ea0dfcbe9c1ae99a9cc9b06ebc5ef7e072a18daf
Sebastiano Baldin <baldin.sebastiano@gmail.com>
2018-06-08 16:35:00 +0200

    Completed autocomplete function

--- FormCheck-In/check-in.js
+++ FormCheck-In/check-in.js
@@ -15,6 +15,7 @@ var documentPlace = $('#documentEmissPlace');
 var list = $(':input').not('button');
 var lang;
 var inputLenght = 0;
+var currentFocus = 0;
 
 $(document).off('.data-api');
 
@@ -38,9 +39,10 @@ $(function () {
     });
     http://onlinecheckin.azurewebsites.net/istes/autocompleteall?term=pro 
 */
-residenceState.on('input', () => {
-    autocomplete(residenceState, 'http://onlinecheckin.azurewebsites.net/istes/autocompletestati');
-})
+
+
+autocomplete(residenceState, 'http://localhost:8080/istes/autocompletestati');
+autocomplete(city, 'http://localhost:8080/istes/autocompletecomuni');
 
 function atome(ref, data) {
     let elem = $('<div></div>');
@@ -59,54 +61,109 @@ function atome(ref, data) {
         elem.append(item);
     }
 
-    function closeAllLists(elmnt) {
-        //close all autocomplete lists in the document,
-        //except the one passed as an argument:
-        let x = $(".autocomplete-items");
-        if (elmnt == null && elmnt == undefined) {
-            x.remove();
-        } else {
-            for (var i = 0; i < getArrayLength(x); i++) {
-                if (elmnt != x[i] && elmnt != residenceState) {
-                    x[i].parent().remove(x[i]);
-                }
-            }
-        }
-    }
-    //execute a function when someone clicks in the document:
-    $('*').on("click", function () {
-        closeAllLists();
-    });
+
 }
 
+residenceState.on('change', () => {
+    if (residenceState.val().toUpperCase() !== 'ITALIA') {
+        $('#toToggle').hide();
+    } else {
+        $('#toToggle').show();
+    }
+})
 
-function autocomplete(inputRef, url) {
-    inputLenght = inputRef.val().length;
-    if (inputLenght >= 2) {
-        $.ajax(url + "?term=" + inputRef.val())
-        .done(function (list) {
-            let elem = $('<div></div>');
-            elem.attr('id', inputRef.attr('id') + "autocomplete-list");
-            elem.attr('class', 'autocomplete-items');
-            inputRef.parent().append(elem);
-            for (let x = 0; x < getArrayLength(list); x++) {
-                let item = $('<div></div>');
-                item.append(`<strong>${list[x].substr(0, inputLenght)}</strong>${list[x].substr(inputLenght)}`);
-                //item.append(list[x]);
-                item.append(`<input type="hidden" value="${list[x]}">`);
-                item.on('click', () => {
-                    inputRef.val(item.children('input').val());
-                    closeAllLists();
-                })
-                elem.append(item);
+function autocomplete(inputRef, urlReq, charMin) {
+
+    inputRef.on('input', () => {
+        inputLenght = inputRef.val().length;
+        if (inputLenght < 1) {
+            closeAllLists();
+            return; 
+        }
+        $.ajax({
+                type: "GET",
+                url: urlReq,
+                data: {
+                    term: inputRef.val()
+                },
+                async: true,
+                dataType: 'json', //you may use jsonp for cross origin request
+                crossDomain: true,
+                success: function (data, status, xhr) {
+                    //alert(data);
+                }
+            })
+            .done(function (listRaw) {
+                var list = JSON.parse(listRaw);
+                closeAllLists();
+                let elem = $('<div></div>');
+                elem.attr('id', inputRef.attr('id') + "autocomplete-list");
+                elem.attr('class', 'autocomplete-items');
+                inputRef.parent().append(elem);
+                for (let x = 0; x < getArrayLength(list); x++) {
+                    let item = $('<div></div>');
+                    item.append(`<strong>${list[x].substr(0, inputLenght)}</strong>${list[x].substr(inputLenght)}`);
+                    //item.append(list[x]);
+                    item.append(`<input type="hidden" value="${list[x]}">`);
+                    item.on('click', () => {
+                        inputRef.val(item.children('input').val());
+                        closeAllLists();
+                    })
+                    elem.append(item);
+                }
+            })
+            .fail(function () {
+                atome(inputRef, ["AMALFI (SA)", "AMANDOLA (AP)", "AMANTEA (CS)", "AMARO (UD)", "AMARONI (CZ)", "AMASENO (FR)", "AMATO (CZ)", "AMATRICE (RI)", "AMBIVERE (BG)", "AMBLAR (TN)", "AMEGLIA (SP)", "AMELIA (TR)", "AMENDOLARA (CS)", "AMENO (NO)", "AMOROSI (BN)", "AMPEZZO (UD)"]);
+            })
+            .always(function () {
+
+            });
+    })
+
+    inputRef.on("keydown", function (e) {
+        let x = $("#" + inputRef.attr('id') + "autocomplete-list");
+        if (x) x = x.children('div');
+        else return;
+        //alert(x.length);
+        if (e.keyCode == 40) {
+            /*If the arrow DOWN key is pressed,
+            increase the currentFocus variable:*/
+            currentFocus++;
+            /*and and make the current item more visible:*/
+            addActive(x);
+        } else if (e.keyCode == 38) { //up
+            /*If the arrow UP key is pressed,
+            decrease the currentFocus variable:*/
+            currentFocus--;
+            /*and and make the current item more visible:*/
+            addActive(x);
+        } else if (e.keyCode == 13) {
+            /*If the ENTER key is pressed, prevent the form from being submitted,*/
+            e.preventDefault();
+            if (currentFocus > -1) {
+                /*and simulate a click on the "active" item:*/
+                if (x) x.get(currentFocus).click();
             }
-        })
-        .fail(function () {
-            atome(inputRef, ["AMALFI (SA)","AMANDOLA (AP)","AMANTEA (CS)","AMARO (UD)","AMARONI (CZ)","AMASENO (FR)","AMATO (CZ)","AMATRICE (RI)","AMBIVERE (BG)","AMBLAR (TN)","AMEGLIA (SP)","AMELIA (TR)","AMENDOLARA (CS)","AMENO (NO)","AMOROSI (BN)","AMPEZZO (UD)"]);
-        })
-        .always(function () {
+        }
+    });
 
-        });
+    function addActive(x) {
+        /*a function to classify an item as "active":*/
+        if (!x) return false;
+        /*start by removing the "active" class on all items:*/
+        removeActive(x);
+        if (currentFocus >= x.length) currentFocus = 0;
+        if (currentFocus < 0) currentFocus = (x.length - 1);
+        /*add class "autocomplete-active":*/
+        $(x.get(x)).addClass('autocomplete-active');
+    }
+
+    function removeActive(x) {
+        /*a function to remove the "active" class from all autocomplete items:*/
+        x.removeClass('autocomplete-active');
+        /* for (var i = 0; i < x.length; i++) {
+            x[i].classList.remove("autocomplete-active");
+        } */
     }
 
     function closeAllLists(elmnt) {
@@ -115,13 +172,14 @@ function autocomplete(inputRef, url) {
         let x = $(".autocomplete-items");
         if (elmnt == null && elmnt == undefined) {
             x.remove();
-        } else {
+            currentFocus = 0;
+        } /* else {
             for (var i = 0; i < getArrayLength(x); i++) {
                 if (elmnt != x[i] && elmnt != residenceState) {
                     x[i].parent().remove(x[i]);
                 }
             }
-        }
+        } */
     }
     //execute a function when someone clicks in the document:
     $('*').on("click", function () {
